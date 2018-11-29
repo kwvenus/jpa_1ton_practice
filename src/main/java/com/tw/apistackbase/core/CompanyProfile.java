710b9924505e8b9ec852ec96739a007d767e79a8
Wong Cho Hing <venus.wong@oocl.com>
2018-11-29 16:50:27 +0800

    One to many practice

---
+++ src/main/java/com/tw/apistackbase/core/CompanyProfile.java
@@ -0,0 +1,36 @@
+package com.tw.apistackbase.core;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class CompanyProfile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private long registeredCapital;
+    private String certId;
+
+    public long getRegisteredCapital() {
+        return registeredCapital;
+    }
+
+    public void setRegisteredCapital(long registeredCapital) {
+        this.registeredCapital = registeredCapital;
+    }
+
+    public String getCertId() {
+        return certId;
+    }
+
+    public void setCertId(String certId) {
+        this.certId = certId;
+    }
+
+
+    public CompanyProfile() {
+    }
+}
